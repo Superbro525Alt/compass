9198e0e1cbb2ee3dd1eb5e2bd407f090819b83c8
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-09-04 19:35:30 +0800

    Added apis for classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { useState } from "react";
+
+
+
 export default function Home() {
+
+  const [classes, setClasses] = useState("");
+
+  function getClasses() {
+    fetch("http://localhost:3000/api/classes").then((res) => {
+        res.text().then((text) => {
+            setClasses(text);
+        });
+    });
+  }
+
+  getClasses();
+
   return (
     <div>
-
+      {classes ? <p>{classes}</p> : <p>Loading...</p>}
     </div>
   )
 }
