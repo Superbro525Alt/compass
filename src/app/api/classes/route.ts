b71a70fecd6f024469edefff7a3719bfaa5c7acd
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-09-04 20:28:18 +0800

    Added some basic rendering code

--- src/app/api/classes/route.ts
+++ src/app/api/classes/route.ts
@@ -1,3 +1,3 @@
 export async function GET(request: Request) {
-  return new Response('[{name: "test_class"}]');
+  return new Response(JSON.stringify([{name: "test_class"}]));
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,30 @@
 "use client";
 
+import React from "react";
 import { useState } from "react";
 
-
+function Container(props: any) {
+    return (
+        <>
+            <div className="container" style={{"width": props.width}}>
+                {props.children.map((child: any) => (
+                    <div>
+                        {child}
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
 
 export default function Home() {
 
-  const [classes, setClasses] = useState("");
+  const [classes, setClasses] = useState([]);
 
   function getClasses() {
     fetch("http://localhost:3000/api/classes").then((res) => {
         res.text().then((text) => {
-            setClasses(text);
+            setClasses(JSON.parse(text));
         });
     });
   }
@@ -19,8 +32,10 @@ export default function Home() {
   getClasses();
 
   return (
-    <div>
-      {classes ? <p>{classes}</p> : <p>Loading...</p>}
+    <div className="app">
+        <Container>
+            {classes ? classes.map((key) => (<p>{key.name}</p>)) : <p>Loading...</p>}
+        </Container>
     </div>
   )
 }
