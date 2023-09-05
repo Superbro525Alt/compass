29e35270c48669cea138843a99b483421ec40a04
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-09-05 12:50:08 +0800

    Finished main UI outline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,45 @@ import { useState } from "react";
 function Container(props: any) {
     return (
         <>
-            <div className="container" style={{"width": props.width, "height": props.height, "maxHeight": props.height}}>
+            <div className={"container " + props.className} style={{"width": props.width, "height": props.height, "maxHeight": props.height}}>
                 {props.children ? props.children.map((child: any) => (
-                    <div>
+                    <>
                         {child}
-                    </div>
+                    </>
                 )) : <div></div>}
             </div>
         </>
+    )
+}
+// how do i make the element go to the top of their container?
+// how do i make the element go to the bottom of their container?
+
+function NavButton(props: any) {
+    return (
+
+            <div className="nav-button">
+                <p style={{margin: "auto auto", textAlign: "center"}}>{props.text}</p>
+            </div>
+
+    )
+}
+
+const NAV_BUTTONS = [
+    "Home",
+    "Classes",
+    "Assignments",
+    "Grades",
+    "Timetable",
+    "Attendance",
+    "Profile",
+    "Settings"
+]
+
+function Class(props: any) {
+    return (
+        <div className="class">
+            <p>{props.name}</p>
+        </div>
     )
 }
 
@@ -34,20 +65,22 @@ export default function Home() {
   return (
     <div className="app">
         <div className="vertical-nav" style={{marginRight: "1%", height: "98%"}}>
-            <Container height="100%"/>
+            <Container height="100%" className="nav-container">
+                    {NAV_BUTTONS ? NAV_BUTTONS.map((key) => (<NavButton text={key}/>)) : <p>Loading...</p>}
+            </Container>
         </div>
         <div className="app-container">
             <div className="row">
                 <Container width="70%">
-                    {classes ? classes.map((key) => (<p>{key.name}</p>)) : <p>Loading...</p>}
+                    {classes ? classes.map((key) => (<Class name={key.name}/>)) : <p>Loading...</p>}
                 </Container>
                 <Container width="30%">
-                    {classes ? classes.map((key) => (<p>{key.name}</p>)) : <p>Loading...</p>}
+                    {classes ? classes.map((key) => (<p>{"Assignments"}</p>)) : <p>Loading...</p>}
                 </Container>
             </div>
             <div className="row">
                 <Container>
-                    {classes ? classes.map((key) => (<p>{key.name}</p>)) : <p>Loading...</p>}
+                    {classes ? classes.map((key) => (<p>{"Notices"}</p>)) : <p>Loading...</p>}
                 </Container>
             </div>
         </div>
